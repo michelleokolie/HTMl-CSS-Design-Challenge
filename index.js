d89214113c447a86ab4ded9c2127fadb00eb9c7c
michelleokolie <michelleokolie11@gmail.com>
2021-06-29 17:50:26 -0500

    js and asset

---
+++ index.js
@@ -0,0 +1,25 @@
+let getText = () => {
+    let message = document.createElement("div");
+    let mesgContainer = document.createElement("div");
+    let mesgText = document.createElement("p");
+    let textValue = document.getElementById("message-prompt");
+
+    mesgContainer.classList.add("y-m-c");
+    message.classList.add("your-message");
+
+
+
+    if (!textValue.value == "") {
+        message.appendChild(mesgText);
+        mesgContainer.appendChild(message);
+
+        mesgText.innerHTML = textValue.value;
+        document.getElementById("user-messages").appendChild(mesgContainer).scrollIntoView();
+        textValue.value = "";
+    } else {
+        alert("Please write a message");
+    }
+
+
+}
+
