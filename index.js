e310618a60f3d4eab44baf135dc5b49eb62e9b43
michelleokolie <michelleokolie11@gmail.com>
2021-06-29 22:39:18 -0500

    fixed active user css selection

--- index.js
+++ index.js
@@ -21,9 +21,15 @@ let getText = () => {
     }
 
 }
-let activeUser = () => {
-    card = document.getElementsByClassName("pinned-card");
-    card.addClassList("active");
-} 
 
+let cardContainer = document.getElementById("pinned-cards");
+let card = cardContainer.getElementsByClassName("pinned-card");
 
+for (let i = 0; i < card.length; i++) {
+    card[i].addEventListener("click", function () {
+        let current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    }
+    )
+}
