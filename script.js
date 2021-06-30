1728f5b653c348ac92909dae8f0d3c058e7b7f38
michelleokolie <michelleokolie11@gmail.com>
2021-06-30 00:25:13 -0500

    minor changes

--- script.js
+++ script.js
@@ -22,14 +22,23 @@ let getText = () => {
 
 }
 
-let cardContainer = document.getElementById("pinned-cards");
+
 let card = cardContainer.getElementsByClassName("pinned-card");
+let currentUser = document.getElementById("user-name");
 
 for (let i = 0; i < card.length; i++) {
     card[i].addEventListener("click", function () {
         let current = document.getElementsByClassName("active");
         current[0].className = current[0].className.replace(" active", "");
         this.className += " active";
+    }
+    )
+}
+for (let i = 0; i < card.length; i++) {
+    card[i].addEventListener("click", function () {
+        let current = card[i];
+        username = document.querySelector(".user-name")
+        currentUser.innerHTML = current.username.innerHTML;
     }
     )
 }
