c7a7e6aba23f1bf4159ead7404246b9928f09556
michelleokolie <michelleokolie11@gmail.com>
2021-07-03 14:37:15 -0500

    added emily, leo and louis. addded chat functions for all three

--- script.js
+++ script.js
@@ -1,20 +1,57 @@
-let getText = () => {
+let getLouisText = () => {
     let message = document.createElement("div");
-    let mesgContainer = document.createElement("div");
     let mesgText = document.createElement("p");
-    let textValue = document.getElementById("message-prompt");
+    let textValue = document.getElementById("louis-prompt");
 
-    mesgContainer.classList.add("y-m-c");
     message.classList.add("your-message");
 
 
 
     if (!textValue.value == "") {
         message.appendChild(mesgText);
-        mesgContainer.appendChild(message);
 
         mesgText.innerHTML = textValue.value;
-        document.getElementById("user-messages").appendChild(mesgContainer).scrollIntoView();
+        document.getElementById("louis-messages").appendChild(message).scrollIntoView();
+        textValue.value = "";
+    } else {
+        alert("Please write a message");
+    }
+
+}
+let getLeoText = () => {
+    let message = document.createElement("div");
+    let mesgText = document.createElement("p");
+    let textValue = document.getElementById("leo-prompt");
+
+    message.classList.add("your-message");
+
+
+
+    if (!textValue.value == "") {
+        message.appendChild(mesgText);
+
+        mesgText.innerHTML = textValue.value;
+        document.getElementById("leo-messages").appendChild(message).scrollIntoView();
+        textValue.value = "";
+    } else {
+        alert("Please write a message");
+    }
+
+}
+let getEmilyText = () => {
+    let message = document.createElement("div");
+    let mesgText = document.createElement("p");
+    let textValue = document.getElementById("emily-prompt");
+
+    message.classList.add("your-message");
+
+
+
+    if (!textValue.value == "") {
+        message.appendChild(mesgText);
+
+        mesgText.innerHTML = textValue.value;
+        document.getElementById("emily-messages").appendChild(message).scrollIntoView();
         textValue.value = "";
     } else {
         alert("Please write a message");
@@ -23,7 +60,8 @@ let getText = () => {
 }
 
 
-let card = cardContainer.getElementsByClassName("pinned-card");
+
+let card = document.getElementsByClassName("pinned-card");
 let currentUser = document.getElementById("user-name");
 
 for (let i = 0; i < card.length; i++) {
@@ -31,14 +69,38 @@ for (let i = 0; i < card.length; i++) {
         let current = document.getElementsByClassName("active");
         current[0].className = current[0].className.replace(" active", "");
         this.className += " active";
+
+        currentUser.innerHTML = document.getElementsByClassName("active")[0].querySelector("p.user-name").innerText;
+
+        
     }
     )
 }
-for (let i = 0; i < card.length; i++) {
-    card[i].addEventListener("click", function () {
-        let current = card[i];
-        username = document.querySelector(".user-name")
-        currentUser.innerHTML = current.username.innerHTML;
-    }
-    )
+let message = () => {
+    alert("This feature is still under construction");
+}
+
+let showLeo = () => {
+    document.getElementById("leo-messages").style.display = "flex";
+    document.getElementById("leo-type").style.display = "flex";
+    document.getElementById("louis-messages").style.display = "none";
+    document.getElementById("louis-type").style.display = "none";
+    document.getElementById("emily-messages").style.display = "none";
+    document.getElementById("emily-type").style.display = "none";
+}
+let showLouis = () => {
+    document.getElementById("leo-messages").style.display = "none";
+    document.getElementById("louis-messages").style.display = "flex";
+    document.getElementById("louis-type").style.display = "flex";
+    document.getElementById("leo-type").style.display = "none";
+    document.getElementById("emily-messages").style.display = "none";
+    document.getElementById("emily-type").style.display = "none";
+}
+let showEmily = () => {
+    document.getElementById("louis-messages").style.display = "none";
+    document.getElementById("louis-type").style.display = "none";
+    document.getElementById("leo-type").style.display = "none";
+    document.getElementById("leo-messages").style.display = "none";
+    document.getElementById("emily-messages").style.display = "flex";
+    document.getElementById("emily-type").style.display = "flex";
 }
